ddb45af41a0d8cd0f4e1f8fe0526ae547469f5be
minhle_3107 <lexuanminh3107@gmail.com>
2024-06-23 14:58:08 +0700

    create develop

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <ErrorBoundary>
       <Provider store={store}>
-        <TheContainer />
+        {/*<TheContainer />*/}
       </Provider>
     </ErrorBoundary>
   );
